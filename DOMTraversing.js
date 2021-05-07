83af4101a7af1f92dad67dfe5ee4b055f983ae0f
NelsonUprety <nelsonuprety3@gmail.com>
2021-05-07 13:03:51 +0545

    learnt about DOM traversing

---
+++ DOMTraversing.js
@@ -0,0 +1,32 @@
+// Dom traversing
+// So dom traversing is basically walking through the dom
+// which means we can select an element based on another element
+
+const h1 = document.querySelector('h1');
+
+// going downwards: selecting child elements
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function(el){
+  if(el !== h1) el.style.transform = 'scale(0.5)';
+});
